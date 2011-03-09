7709884a14cb77b947f45315e6a25713af35c3ed
ianwookim <ianwookim@gmail.com>
2011-03-09 14:13:59 +0000

    HROOT module export all other submodules.

--- src/HROOT.hs
+++ src/HROOT.hs
@@ -1,7 +1,11 @@
 module HROOT (
+  module HROOT.Type,
   module HROOT.Class,
-  module HROOT.Function 
+  module HROOT.Function, 
+  module HROOT.AddOn
 ) where
 
+import HROOT.Type
 import HROOT.Class
 import HROOT.Function
+import HROOT.AddOn 
