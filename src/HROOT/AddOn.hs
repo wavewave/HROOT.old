44aa079b5da2d058da72773f36566a8ddbfffa08
ianwookim <ianwookim@gmail.com>
2011-09-28 12:47:49 +0000

    make ProjectionX work (by upcastTH2 ) and add project1d.hs example.

--- src/HROOT/AddOn.hs
+++ src/HROOT/AddOn.hs
@@ -1,6 +1,15 @@
-{-# LANGUAGE TypeFamilies, GADTs, ExistentialQuantification, EmptyDataDecls #-}
+{-# LANGUAGE TypeFamilies, GADTs, ExistentialQuantification, EmptyDataDecls, ScopedTypeVariables #-}
 
 module HROOT.AddOn where
+
+import Foreign.ForeignPtr
+import HROOT.Class.Interface
+
+upcastTH2 :: (FPtr a, ITH2 a) => a -> TH2 
+upcastTH2 h = let fh = get_fptr h
+                  fh2 :: ForeignPtr RawTH2 =  castForeignPtr fh 
+              in cast_fptr_to_obj fh2  
+
 
 {-
 --import Foreign.C            
