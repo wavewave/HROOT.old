1eb27a9bd5eb249781ff8eafb06cf3028e73834d
ianwookim <ianwookim@gmail.com>
2011-10-07 18:37:51 +0000

    matched with version 0.6.6

--- src/HROOT/AddOn.hs
+++ src/HROOT/AddOn.hs
@@ -1,13 +1,4 @@
-{-# LANGUAGE TypeFamilies, GADTs, ExistentialQuantification, EmptyDataDecls, ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies, GADTs, ExistentialQuantification, EmptyDataDecls, ScopedTypeVariables, FlexibleInstances #-}
 
 module HROOT.AddOn where
 
-{-
-import Foreign.ForeignPtr
-import HROOT.Class.Interface
-
-upcastTH2 :: (FPtr a, ITH2 a) => a -> TH2 
-upcastTH2 h = let fh = get_fptr h
-                  fh2 :: ForeignPtr RawTH2 =  castForeignPtr fh 
-              in cast_fptr_to_obj fh2  
--}
